a2dfaf353f93682ae313ce0890167422337ec058
STUDENT Yit How Wong <Yit.How.Wong@students.ittralee.ie>
2023-03-30 17:08:37 +0100

    Added new line

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello_world():
     return 'Hello World'
+    return 'Second_hello_world'
     
 if __name__ == '__main__':
     application.run(debug=True)
