4c71c5c3692b44a36b53bd473e25f55361fdf722
STUDENT Yit How Wong <Yit.How.Wong@students.ittralee.ie>
2023-03-31 11:43:39 +0100

    using render_template

--- application.py
+++ application.py
@@ -9,8 +9,8 @@ import zipfile
 application = Flask(__name__)
 @application.route('/')
 def home():
-    #return render_template('index.html')
-    return "hello world"
+    return render_template('index.html')
+    #return "hello world"
     
 if __name__ == '__main__':
     application.run(debug=True)
