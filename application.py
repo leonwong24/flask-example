2bc1e7e50d086a48aa22ba6c801bdac8df5b0130
STUDENT Yit How Wong <Yit.How.Wong@students.ittralee.ie>
2023-03-31 11:24:51 +0100

    update application.py

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, send_file
-import cv2
-import numpy as np
-import os
-import glob
-import zipfile
+#from flask import Flask, render_template, request, send_file
+from flask import Flask
+#import cv2
+#import numpy as np
+#import os
+#import glob
+#import zipfile
 #application = Flask(__name__, template_folder="templates")
 application = Flask(__name__)
 @application.route('/')
