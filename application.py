0fa680f52ebaa201bb08a411c905ce3696911c27
STUDENT Yit How Wong <Yit.How.Wong@students.ittralee.ie>
2023-03-31 11:22:26 +0100

    update application.py

--- application.py
+++ application.py
@@ -4,12 +4,12 @@ import numpy as np
 import os
 import glob
 import zipfile
-application = Flask(__name__, template_folder="templates")
-
+#application = Flask(__name__, template_folder="templates")
+application = Flask(__name__)
 @application.route('/')
 def home():
-    return render_template('index.html')
-
+    #return render_template('index.html')
+    return "hello world"
     
 if __name__ == '__main__':
     application.run(debug=True)
